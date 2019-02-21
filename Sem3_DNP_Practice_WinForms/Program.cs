a2c931d1f66427a4604748da1f37f7cab50585a2
Andrei Mungiu <andrei_mungiu@yahoo.com>
2019-02-21 09:35:42 +0100

    Add project files.

---
+++ Sem3_DNP_Practice_WinForms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sem3_DNP_Practice_WinForms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
